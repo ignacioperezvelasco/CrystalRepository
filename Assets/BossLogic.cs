8f2bdcb4bfa2a5159e07b030804f4caa1eba36ad
Sergio E <Sergio.escuder5@gmail.com>
2021-03-23 18:36:38 +0100

    Telegraphing & Rock Attack

--- Assets/BossLogic.cs
+++ Assets/BossLogic.cs
@@ -21,33 +21,116 @@ public class BossLogic : MonoBehaviour
 {
 
     #region VARIABLES
+    public enum AttackType
+    {
+        NONE,
+        CHARGE_ATTACK,
+        ROCK_ATTACK
+    };
+
     AreaType playerCurrentArea;
     AreaType bossCurrentArea;
+
+    Transform player;
+
+    public AttackType currentAttack;
+
     [Header("AREAS")]
     [SerializeField] Transform[] areasPosition;
-    [Header("ATTACK VALUES")]
+
+    [Header("CHARGE ATTACK")]
     [SerializeField] float timePreparingChargeAttack = 1.5f;
     [SerializeField] float speedAttack = 0.5f;
     [SerializeField] float timeBetweenAttacks = 5;
+
+    [Header("TELEGRAPHING")]
+    [SerializeField] Transform startTelegraphing;
+    [SerializeField] Transform endTelegraphing;
+    LineRenderer line;
+
+    [Header("ROCK ATTACK")]
+    [SerializeField] GameObject rock;
+    [SerializeField] Transform rockSpawner;
+    [SerializeField] float rockSpeed = 0.75f;
+    [SerializeField] float heightRock = 5;
+
     float timerAttack = 0;
+    #endregion
+
+
+    #region START
+    private void Start()
+    {
+        //Buscamos al player
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        //Buscamos el LineRenderer
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+    }
     #endregion
 
     #region UPDATE
     void Update()
     {
+        //Seeteamos las posiciones del telgraphing
+        line.SetPosition(0, startTelegraphing.position);
+        line.SetPosition(1, endTelegraphing.position);
+
         timerAttack += Time.deltaTime;
         if (timerAttack >= timeBetweenAttacks)
         {
             timerAttack = 0;
 
-            //Miramos hacia el siguiente area
-            this.transform.DOLookAt(areasPosition[(int)playerCurrentArea].position, timePreparingChargeAttack);
+            //Controlameos el ataque
+            AttackBehaviourHandler();
+        }
+    }
+    #endregion
 
-            //Seteamos la siguiente area
-            bossCurrentArea = playerCurrentArea;
+    #region ATTACK BEHAVIOUR HANDLER
+    void AttackBehaviourHandler()
+    {
+        switch (currentAttack)
+        {
+            case AttackType.NONE:
+                {
+                    break;
+                }
+            case AttackType.CHARGE_ATTACK:
+                {
+                    //Activamos el telegraphing
+                    float distanceToArea = Vector3.Distance(this.transform.position, areasPosition[(int)playerCurrentArea].position);
+                    Vector3 newEndTelepgraphing = new Vector3(startTelegraphing.position.x + distanceToArea, startTelegraphing.position.y,  startTelegraphing.position.z);
 
-            //Llamamos a la funcion de atacar
-            Invoke("ChargeAttack", timePreparingChargeAttack);
+                    endTelegraphing.position = newEndTelepgraphing;
+
+                    
+
+                    //Miramos hacia el siguiente area
+                    this.transform.DOLookAt(areasPosition[(int)playerCurrentArea].position, timePreparingChargeAttack);
+
+                    //Seteamos la siguiente area
+                    bossCurrentArea = playerCurrentArea;
+
+                    //Activamos el telegraphing
+                    Invoke("ActiveTelegraphing", timePreparingChargeAttack);
+
+                    //Llamamos a la funcion de atacar
+                    Invoke("ChargeAttack", timePreparingChargeAttack + 0.5f);
+                    break;
+                }
+            case AttackType.ROCK_ATTACK:
+                {
+                    //Miramos al jugador
+                    Vector3 toLookAt = new Vector3(player.position.x, this.transform.position.y, player.position.z);
+                    this.transform.LookAt(toLookAt);
+
+                    RockAttack();
+                    break;
+                }
+            default:
+                break;
         }
     }
     #endregion
@@ -70,7 +153,33 @@ public class BossLogic : MonoBehaviour
         {
             this.transform.DOMove(areasPosition[(int)bossCurrentArea].position, speedAttack);
         }
+
+        DeactiveTelegraphing();
         
+    }
+    #endregion
+
+    #region ACTIVE TELEGRAPHING
+    void ActiveTelegraphing()
+    {
+        line.enabled = true;
+    }
+    #endregion
+
+    #region DEACTIVE TELEGRAPHING
+    void DeactiveTelegraphing()
+    {
+        line.enabled = false;
+    }
+    #endregion
+
+    #region ROCK ATTACK
+    void RockAttack()
+    {
+        GameObject go = Instantiate( rock, rockSpawner.position, rockSpawner.rotation) as GameObject;
+
+        go.transform.DOJump(player.position, heightRock, 1,rockSpeed);
+        Destroy(go, rockSpeed);
     }
     #endregion
 
